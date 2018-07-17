cf0c6352a4585547fcc7271d454e5b2694b73ac5
limengtong <limengtong@iyunxiao.com>
2018-07-17 21:35:53 +0800

    my comments

--- design_pattern/单例模式/singleleton/src/singleleton/LazySafeFaster.java
+++ design_pattern/单例模式/singleleton/src/singleleton/LazySafeFaster.java
@@ -3,13 +3,14 @@ package singleleton;
 public class LazySafeFaster {
 	public static LazySafeFaster lsf;
 	private LazySafeFaster(){};
+	//多线程情况下 两个 if 保证更高效
 	public synchronized static LazySafeFaster getInstance(){
 		if(lsf==null){
 			synchronized (LazySafeFaster.class) {
 				if(lsf==null){
 					lsf = new LazySafeFaster();
 				}
-			}			
+			}
 		}
 		return lsf;
 	}
