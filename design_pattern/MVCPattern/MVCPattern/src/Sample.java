755cc56063a538e7a142182c5e575dd6f2516c13
limengtong <limengtong@iyunxiao.com>
2018-07-26 17:41:49 +0800

    mvc pattern

---
+++ design_pattern/MVCPattern/MVCPattern/src/Sample.java
@@ -0,0 +1,18 @@
+
+public class Sample {
+	public static void main(String args[]) {
+		Student model = retriveStudentFromDatabase();
+		StudentView view = new StudentView();
+		StudentController controller = new StudentController(model, view);
+		controller.updateView();
+		controller.setStudentName("John");
+		controller.updateView();
+	}
+
+	private static Student retriveStudentFromDatabase() {
+		Student student = new Student();
+		student.setName("Robert");
+		student.setRollNo("10");
+		return student;
+	}
+}
---
+++ design_pattern/MVCPattern/MVCPattern/src/StudentView.java
@@ -0,0 +1,8 @@
+
+public class StudentView {
+	public void showStudentData(String studentName,String studentRollNo){
+		System.out.println(" Student: ");
+		System.out.println(" Student name: " + studentName);
+		System.out.println(" Student rollNo: " + studentRollNo);
+	}
+}
